ba2e629ea771e45348722e5e964b42e8987ed72d
Deepak <deepakkushwaha5945@gmail.com>
2026-03-23 17:41:14 +0530

    loader updated

--- src/common/Login.jsx
+++ src/common/Login.jsx
@@ -64,6 +64,7 @@ export default function Login() {
                         }
                         else {
                             toast.error(finalRes.message)
+                            setloading(false)
                         }
                     })
 
