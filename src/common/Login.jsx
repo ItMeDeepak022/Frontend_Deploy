2d87097b2836080608cdb005908bc854cb4ca91b
Deepak <deepakkushwaha5945@gmail.com>
2026-03-23 18:21:13 +0530

    Updated UI

--- src/common/Login.jsx
+++ src/common/Login.jsx
@@ -78,130 +78,11 @@ export default function Login() {
     return (
         < >
             <ToastContainer />
-            {/* <div className="min-h-screen flex items-center justify-center bg-gray-100">
-                <h1 className='absolute md:text-4xl text-[25px] text-orange-700 font-semibold md:top-10 top-0 md:block hidden'>Welcome to the Enquiry Management System</h1>
 
-                <div className="bg-white p-8 rounded-lg shadow-md w-[400px]  max-w-md m-3">
-                    <h2 className="text-3xl font-bold mb-6 text-center">
-                        {isLogin ? "Welcome Back 👋" : "Create an Account"}
-                    </h2>
-                    <form onSubmit={loginDone} className="space-y-6 w-full">
-                        {!isLogin && (
-                            <div>
-                                <label className="block mb-1 text-gray-700" htmlFor="name">
-                                    Name
-                                </label>
-                                <input
-                                    type="text"
-                                    name="name"
-                                    id="name"
-                                    className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
-                                    placeholder="Enter your name"
-                                    required
-                                />
-                            </div>
-                        )}
-                        <div>
-                            <label className="block mb-1 text-gray-700" htmlFor="email">
-                                Email
-                            </label>
-                            <input
-                                type="email"
-                                name="email"
-                                id="email"
-                                className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
-                                placeholder="Enter your email"
-                                required
-                            />
-                        </div>
-                        <div>
-                            <label className="block mb-1 text-gray-700" htmlFor="password">
-                                Password
-                            </label>
-                            <input
-                                type="password"
-                                name="password"
-                                id="password"
-                                className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
-                                placeholder="Enter your password"
-                                required
-                            />
-                        </div>
-                        <button
-                            type="submit"
-                            className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition"
-                        >
-                            {isLogin ?
-                                <div className='flex items-center justify-center gap-5' >
-                                    <p className='text-white text-[18px]'>Login</p>
-
-                                    {
-                                        loding && (
-                                            <div class="flex items-center justify-center">
-                                                <div class="w-7 h-7 rounded-full animate-spin
-                    border-4 border-solid border-white border-t-transparent"></div>
-
-                                            </div>
-                                        )
-
-                                    }
-
-                                </div>
-
-                                :
-                                <div className='flex items-center justify-center gap-5' >
-                                    <p className='text-white text-[18px]'>Sign Up</p>
-
-                                    {
-                                        loding && (
-                                            <div class="flex items-center justify-center">
-                                                <div class="w-7 h-7 rounded-full animate-spin
-                    border-4 border-solid border-white border-t-transparent"></div>
-
-                                            </div>
-                                        )
-
-                                    }
-
-                                </div>
-
-                            }
-
-                        </button>
-                    </form>
-                    <div className="mt-4 text-center">
-                        <button
-                            className="text-blue-600 "
-                            onClick={() => setIsLogin(!isLogin)}
-                            type="button"
-                        >
-                            {isLogin
-                                ?
-
-                                < div >
-                                    <p className='hover:underline'> "Don't have an account? Sign Up"</p>
-                                    <p className='text-blue-600 hover:underline'><Link to={"/forget-password"}>Forgot Password</Link> </p>
-                                </div>
-                                :
-                                <div>
-                                    <p className='hover:underline'>Already have an account? Login</p>
-                                     
-                                </div>
-
-
-                            }
-                        </button>
-
-                    </div>
-                    <ToastContainer />
-                </div >
-            </div > */}
 
             <div className="min-h-screen  flex items-center justify-center  bg-gray-200 relative overflow-hidden px-3">
 
-                {/* 🔥 BACKGROUND GLOW */}
-                {/* <div className="absolute w-[300px] h-[300px] md:w-[500px] md:h-[500px] bg-blue-500 opacity-30 blur-[120px] rounded-full top-[-80px] left-[-80px]"></div> */}
-                {/* <div className="absolute w-[250px] h-[250px] md:w-[400px] md:h-[400px] bg-purple-500 opacity-30 blur-[120px] rounded-full bottom-[-80px] right-[-80px]"></div> */}
+
 
                 <div className=" shadow-lg w-full max-w-6xl backdrop-blur-xl bg-white border border-white/20 rounded-2xl md:rounded-3xl shadow-2xl flex flex-col md:flex-row overflow-hidden">
 
@@ -252,7 +133,7 @@ export default function Login() {
                                         name="name"
                                         placeholder="Enter your name"
                                         required
-                                        className="w-full mt-1 px-3 md:px-4 py-2 rounded-lg bg-[#E8F0FE] border border-white/20 focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder-gray-400 text-sm"
+                                        className="w-full mt-1 px-3 md:px-4 py-2 rounded-lg bg-[#E8F0FE] border border-white/20 focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder:text-black text-sm"
                                     />
                                 </div>
                             )}
@@ -263,8 +144,7 @@ export default function Login() {
                                     type="email"
                                     name="email"
                                     placeholder="Enter your email"
-                                    className="w-full mt-1 px-3 md:px-4 py-2 rounded-lg bg-slate-200 border border-white/20 focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder-gray-400 text-sm"
-                                    required
+                                    className="w-full mt-1 px-3 md:px-4 py-2 rounded-lg bg-[#E8F0FE] border border-white/20 focus:outline-none focus:ring-2 focus:ring-blue-500  placeholder-red-700 text-sm"
                                 />
                             </div>
 
@@ -274,15 +154,14 @@ export default function Login() {
                                     type="password"
                                     name="password"
                                     placeholder="Enter your password"
-                                    className="w-full mt-1 px-3 md:px-4 py-2 rounded-lg bg-white/10 border border-white/20 focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder-gray-400 text-sm"
+                                    className="w-full mt-1 px-3 md:px-4 py-2 rounded-lg bg-[#E8F0FE] border border-white/20 focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder:text-black text-sm"
                                     required
                                 />
                             </div>
 
-                            {/* BUTTON */}
                             <button
                                 type="submit"
-                                className="w-full  bg-blue-500   py-2 rounded-lg font-semibold  text-white  flex items-center justify-center gap-5 shadow-lg text-sm md:text-base"
+                                className="w-full bg-blue-500 py-2 rounded-lg font-semibold text-white flex items-center justify-center gap-5 shadow-lg text-sm md:text-base"
                             >
                                 <span>{isLogin ? "Login" : "Sign Up"}</span>
 
@@ -298,7 +177,7 @@ export default function Login() {
                             <button
                                 onClick={() => setIsLogin(!isLogin)}
                                 type="button"
-                                 
+
                             >
                                 {isLogin ? (
                                     <div>
